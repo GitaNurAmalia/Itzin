00bd9d5bef5dc24d7cb1f2e8510c676616fadb5d
GitaNurAmalia <nuramaliagita24@gmail.com>
2026-05-20 10:53:50 +0700

    first commit

---
+++ lib/core/supabase_config.dart
@@ -0,0 +1,3 @@
+const String supabaseUrl = 'https://puryjnhtxklfqfqjkizd.supabase.co';
+const String supabaseAnonKey = 'sb_publishable_jJZXXdVWXFfS1zDwjVtseQ_8n2wZMxj';
+const String supabaseServiceRoleKey = 'sb_secret_EN0yCy2mGQdaw_cQ5RKfSw_t6GMNlHI';
